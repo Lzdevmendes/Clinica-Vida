db3ea3b32ca3ea34a056aa1a666462bc1dc08b4a
Lzmendesdev <Lzmendestechdev@gmail.com>
2025-09-11 17:47:55 -0300

    Create_security_and_JWT

---
+++ app/utils/jwt_hendler.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from app.config import settings
+
+def create_acess_token(data: dict, expires_minutes: int | None = None):
+  to_encode = data.copy()
+  expire = datetime.utcnow() + timedelta(minutes=expires_minutes or settings.ACESS_TOKEN_EXPIRE_MINIUTES)
+  to_encode.update({"exp": expire})
+  encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+  return encoded_jwt
+
+def verify_token(token: str):
+  try:
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    return payload
+  except JWTError:
+    return None
+  
