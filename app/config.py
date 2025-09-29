8ff3518170ceaffdb8afaa03f2b0ba8143c79942
Lzmendesdev <Lzmendestechdev@gmail.com>
2025-09-29 11:36:13 -0300

    Feat: Clean and file config.py

--- app/config.py
+++ app/config.py
@@ -1,13 +1,19 @@
 from pydantic_settings import BaseSettings
-class Settings(BaseSettings):
-  DATABASE_URL: str
-  SECRET_KEY: str
-  ALGORITHM: str = "HS256"
-  ACESS_TOKEN_EXPIRE_MINUTES: int = 30
-  MAIL_USERNAME: str | None = None
-  MAIL_PASSWORD: str | None = None
 
-class Config:
-  env_file = ".env"
-  
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    MAIL_USERNAME: str | None = None
+    MAIL_PASSWORD: str | None = None
+    MAIL_FROM: str | None = None
+    MAIL_PORT: int | None = None
+    MAIL_SERVER: str | None = None
+    sqlalchemy_url: str | None = None 
+    
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
 settings = Settings()
